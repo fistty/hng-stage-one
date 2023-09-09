b3204795351f878e3e17a84f26172ca27fda5ce4
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-09 11:05:44 +0100

    time functionality

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 let time;
+let defaultInterval;
 const utcElement = document.querySelector(".utc-milli");
+
+const setDate = () => {
+	defaultInterval = setInterval(() => {
+		utcElement.innerText = `UTC TIME IN MILLISECONDS: ${new Date().getTime()}`;
+	}, 1000);
+};
+
 const getDate = async () => {
 	try {
 		const res = await fetch("http://worldtimeapi.org/api/ip");
 		const data = await res.json();
 		time = data.datetime;
 		time = new Date(time).getTime();
-		console.log(time);
+		// console.log(time);
+		// Starts the interval
+		onInterval();
 	} catch (err) {
 		console.log(err);
 	}
@@ -14,13 +24,19 @@ const getDate = async () => {
 
 const onInterval = () => {
 	setInterval(() => {
-		utcElement.innerText = new Date().getTime();
+		if (time) {
+			//Add 1000 milliseconds (1 second) to prevent calling the api more than once
+			time += 1000;
+			utcElement.innerText = `UTC TIME IN MILLISECONDS: ${time}`;
+		}
 	}, 1000);
 };
 
 const onLoad = () => {
-	getDate();
-	onInterval();
+	setDate();
+	// 	console.log(defaultInterval);
+	// 	getDate;
+	// 	clearInterval(defaultInterval);
 };
 
 document.onload = onLoad();
