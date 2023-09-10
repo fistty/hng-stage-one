83ab445a54ecfdd72d5d4fdbbaa9b679879656c2
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-09-10 14:40:12 +0100

    improvements

--- main.js
+++ main.js
@@ -3,18 +3,18 @@ let currentTime;
 let timeInterval;
 const dayElement = document.querySelector(".current-day");
 const timeElement = document.querySelector(".utc-milli");
+const daysOfWeek = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+];
 
 // Sets the day and time before fetching from the api
 const setDefaultDayAndTime = () => {
-	const daysOfWeek = [
-		"Sunday",
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thursday",
-		"Friday",
-		"Saturday",
-	];
 	let day = daysOfWeek[new Date().getUTCDay()];
 	timeInterval = setInterval(() => {
 		dayElement.innerText = `DAY OF THE WEEK: ${day}`;
@@ -22,11 +22,13 @@ const setDefaultDayAndTime = () => {
 	}, 1000);
 };
 
+setDefaultDayAndTime();
+
 const getDefaultDayAndTime = async () => {
 	try {
 		const res = await fetch("https://worldtimeapi.org/api/ip");
 		const data = await res.json();
-		currentDay = new Date(data.datetime).getDay();
+		currentDay = data.day_of_week;
 		currentTime = new Date(data.datetime).getTime();
 
 		// Starts the interval
@@ -37,19 +39,22 @@ const getDefaultDayAndTime = async () => {
 };
 
 const onInterval = () => {
-	setInterval(() => {
-		if (currentDay && currentTime) {
-			clearInterval(timeInterval);
-			//Add 1000 milliseconds (1 second) to prevent calling the api more than once
+	if (currentDay >= 0) {
+		// 0 because day is 0 - 6
+		dayElement.innerText = `DAY OF THE WEEK: ${daysOfWeek[currentDay]}`;
+	}
+
+	if (currentTime) {
+		setInterval(() => {
+			clearInterval(timeInterval); //Add 1000 milliseconds (1 second) to prevent calling the api more than once
 			currentTime += 1000;
-			dayElement.innerText = `DAY OF THE WEEK: ${currentDay}`;
+			dayElement.innerText = `DAY OF THE WEEK: ${daysOfWeek[currentDay]}`;
 			timeElement.innerText = `UTC TIME IN MILLISECONDS: ${currentTime}`;
-		}
-	}, 1000);
+		}, 1000);
+	}
 };
 
 const onLoad = () => {
-	setDefaultDayAndTime();
 	getDefaultDayAndTime();
 };
 
